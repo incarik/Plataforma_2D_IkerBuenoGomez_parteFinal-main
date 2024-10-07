5d90e5c0c30a248079e2927f3a1459985ff9ae4e
incarik <incarik1@gmail.com>
2024-10-07 12:01:55 +0200

    coin

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private bool interactable;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && interactable)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            interactable = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            interactable = false;
+        }
+    }
+}
