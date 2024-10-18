488937bbdb9a0b95326539b3eb44c5049aa673bb
incarik <incarik1@gmail.com>
2024-10-18 10:53:00 +0200

    vida

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Star : MonoBehaviour
 {
+    private bool interactable;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.E) && interactable)
+        {
+            GameManager.instance.AddStar();
 
+            Destroy(gameObject);
+
+            SoundManager.instance.PlaySFX(SoundManager.instance._audioSource ,SoundManager.instance.coinAudio);
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            interactable = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            interactable = false;
+        }
     }
 }
